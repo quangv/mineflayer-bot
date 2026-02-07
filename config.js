d9f616dcdb91ce7fd7dceca5bf5449de1ceda0b7
Quang Van <quang@absentsoft.com>
2026-02-07 18:36:34 -0500

    wip fix localhost

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ import "dotenv/config";
 export default {
   bot: {
     username: process.env.BOT_USERNAME || "FriendlyBot",
-    host: process.env.BOT_HOST || "localhost",
+    host: process.env.BOT_HOST || "127.0.0.1",
     port: parseInt(process.env.BOT_PORT, 10) || 25565,
     version: process.env.BOT_VERSION || "1.20.4",
     auth: process.env.BOT_AUTH || undefined,
